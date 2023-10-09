1672842c9cd3f389730f2b6ea8585c9ad5fe4ab3
apurvaBr11 <apurvkashyappurnea@gmail.com>
2023-10-09 07:37:36 +0530

    vercel setting

--- project/project/settings.py
+++ project/project/settings.py
@@ -140,7 +140,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 
